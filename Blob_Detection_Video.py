dfb0db32b55b57ae65719fc65a07775dd34e0367
Jasmeet Singh <jasmeet0915@gmail.com>
2019-06-08 03:18:20 +0530

    masked frame with only traced path

--- Blob_Detection_Video.py
+++ Blob_Detection_Video.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 points = []
-cap = cv2.VideoCapture("wand circle video 2.mp4")
+cap = cv2.VideoCapture("wand W video.mp4")
 
 params = cv2.SimpleBlobDetector_Params()
 
@@ -15,15 +15,15 @@ params.blobColor = 255
 
 # filter by circularity
 params.filterByCircularity = 1
-params.minCircularity = 0.5
+params.minCircularity = 0.68
 
 # filter by area
 params.filterByArea = 1
 params.minArea = 400
-params.maxArea = 1500
+# params.maxArea = 1500
 
 detector = cv2.SimpleBlobDetector_create(params)
-count = 0
+maxlen = 90
 
 while True:
     _, frame = cap.read()
@@ -31,12 +31,16 @@ while True:
     keypoints = detector.detect(frame)
     frame_with_keypoints = cv2.drawKeypoints(frame, keypoints, np.array([]), (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
     if len(keypoints) != 0:
+        if len(points) > maxlen:
+            points = []
         points_array = cv2.KeyPoint_convert(keypoints)
         points.append(points_array[0])
-        count = count + 1
-        if count > 1:
+        if len(points) > 1:
             for i in range(1, len(points)):
-                cv2.line(frame_with_keypoints, tuple(points[i-1]), tuple(points[i]), (0, 255, 0), 5)
+                cv2.line(frame_with_keypoints, tuple(points[i-1]), tuple(points[i]), (0, 255, 0), 15)
+    lower_green = np.array([0, 255, 0])
+    upper_green = np.array([0, 255, 0])
+    frame_with_keypoints = cv2.inRange(frame_with_keypoints, lower_green, upper_green)
     cv2.imshow("real video", frame)
     cv2.imshow("video with blobs", frame_with_keypoints)
     if cv2.waitKey(1) & 0xFF == ord('q'):
